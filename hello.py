c96d744ba33d43e8abe3edcdad3f9283b9d39af0
rudreshmd <rudreshmd90@gmail.com>
2022-12-22 20:58:58 +0530

    2nd commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
@@ -9,7 +9,7 @@ app.config['SECRET_KEY']="sarojamma"
 
 #create a form class
 class NameForm(FlaskForm):
-	name = StringField("what is your NameForm",validators=[DataRequired()])
+	name = StringField("what is your Name",validators=[DataRequired()])
 	submit = SubmitField("Submit")
 
 
@@ -53,5 +53,6 @@ def name():
 	if form.validate_on_submit():
 		name = form.name.data
 		form.name.data = ''
+		flash("Form Submitted Successfully")
 	return render_template("name.html",name=name,form=form)
 
